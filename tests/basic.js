4415be31a2290636fee368aa9393563e25ab6d84
Christopher Buteau <cbuteau@gmail.com>
2021-01-16 13:12:39 -0500

    need webpack

---
+++ tests/basic.js
@@ -0,0 +1,28 @@
+
+let alliances = require('./');
+
+function SomeObject() {}
+
+SomeObject.prototype = {
+  setup: function() {
+
+  },
+  shutdown: function(force) {
+
+  }
+};
+
+
+let InterfaceSetup = {
+  setup: function() {},
+  shutdown: function(force) {},
+}
+
+describe('Easy tests...', function() {
+
+  it ('interface', function() {
+    let test = new SomeObject();
+    expect(alliances.interface(test, InterfaceSetup)).toBe(true);
+  });
+
+});
