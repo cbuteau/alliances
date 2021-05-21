a1483fd7ce2f444055241ad51d20d77d41743e51
Christopher Buteau <cbuteau@gmail.com>
2021-05-21 15:50:09 -0400

    still not working...expose lib as singleton

--- index.js
+++ index.js
@@ -3,23 +3,36 @@ const INTERFACE_OPTIONS = {
   excludePrivates: false
 };
 
-function Alliances() {}
+var instance = null;
+
+function Alliances() {
+  if (!instance) {
+    instance = new Alliances();
+  }
+  return instance;
+}
 
 Alliances.prototype = {
   interface: function(object, interface, options) {
     let actual = Object.assign(INTERFACE_OPTIONS, options)
     let map = TypeMap(object);
     let int = new TypeMap(interface);
-    
+
   },
   data: function(dataobject, format) {
 
   }
 };
 
-var instance;
-if (!instance) {
-  instance = new Alliances();
-}
 
-module.exports = instance;
+module.exports = {
+  interface: function(object, interface, options) {
+    let actual = Object.assign(INTERFACE_OPTIONS, options)
+    let map = TypeMap(object);
+    let int = new TypeMap(interface);
+
+  },
+  data: function(dataobject, format) {
+
+  }
+};
--- out/bundle.js
+++ out/bundle.js
@@ -1,29 +1,42 @@
-(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.alliances = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 
 const INTERFACE_OPTIONS = {
   excludePrivates: false
 };
 
-function Alliances() {}
+var instance = null;
+
+function Alliances() {
+  if (!instance) {
+    instance = new Alliances();
+  }
+  return instance;
+}
 
 Alliances.prototype = {
   interface: function(object, interface, options) {
     let actual = Object.assign(INTERFACE_OPTIONS, options)
     let map = TypeMap(object);
     let int = new TypeMap(interface);
-    
+
   },
   data: function(dataobject, format) {
 
   }
 };
 
-var instance;
-if (!instance) {
-  instance = new Alliances();
-}
 
-module.exports = instance;
+module.exports = {
+  interface: function(object, interface, options) {
+    let actual = Object.assign(INTERFACE_OPTIONS, options)
+    let map = TypeMap(object);
+    let int = new TypeMap(interface);
+
+  },
+  data: function(dataobject, format) {
+
+  }
+};
 
 },{}],2:[function(require,module,exports){
 
@@ -196,4 +209,5 @@ function getTypeCode(value) {
 
 module.exports = getTypeCode;
 
-},{"./TypeCodes":4}]},{},[6,2,3,4,5,1]);
+},{"./TypeCodes":4}]},{},[6,2,3,4,5,1])(6)
+});
